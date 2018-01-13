3783e8a68a5031d25b37569971f5a9dc1ea802d9
ArKonLab <arkonlab@outlook.com>
2018-01-13 17:53:50 +0300

    Добавлено имя холсту

--- OldSovietGames/WaitForIt/MainWindow.xaml.cs
+++ OldSovietGames/WaitForIt/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Windows.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,62 +20,25 @@ namespace WaitForIt
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-        Int32 t = 0;
-        DispatcherTimer timer = new DispatcherTimer();
-        
-
-        public MainWindow()
+		public MainWindow()
 		{
 			InitializeComponent();
-            
-        }
+		}
 
-        private void timer_tick(object sender, EventArgs e)
-        {
-            #region Tick                            
-            //Внутрь регулярного выражения нужно внести что мы должны выполнить при каждом тике: 
-            //обновить картинку, обновить положения яиц, сменить позицию руки волка
-            t += 1;
-            Label1.Content = t.ToString();                      //Выводим переменную t, Которая инкрементируется с каждым периодом таймера
-
-
-
-
-            #endregion
-        }
-
-        private void BtnExit_Click(object sender, RoutedEventArgs e)
+		private void BtnExit_Click(object sender, RoutedEventArgs e)
 		{
 			this.Close();
 		}
 
-		private void BtnPlayA_Click(object sender, RoutedEventArgs e)   //Игра A
+		private void BtnPlayA_Click(object sender, RoutedEventArgs e)
 		{
-            Timer(100);                                        //инициализируем и запускаем таймер
-            #region GameA
-            //Задаем параметры игры А
 
+		}
 
-            #endregion
-        }
-        
-		private void BtnPlayB_Click(object sender, RoutedEventArgs e)   //Игра B
+		private void BtnPlayB_Click(object sender, RoutedEventArgs e)
 		{
-            Timer(100);                                        //инициализируем и запускаем таймер
-            #region GameB
-            //Задаем параметры игры B
 
-
-            #endregion
-        }
-        private void Timer(int timeKoef)                                //Метод в котором запускаем таймер
-        {
-            
-            timer.Tick += new EventHandler(timer_tick);     //С каждым периодом таймера у нас запускается приватный метод timer_tick
-            timer.Interval = new TimeSpan(100000*timeKoef); //Задали интервал таймера 10 000 000 тиков, что соотвнетствует 1 секунде
-            timer.Start();                                  //Запуск таймера. Чтобы увеличить частоту обновлений нужно уменьшить период тиков
-                                                            //timerKoef
-        }                       
+		}
 
 		private void BtnSound_Click(object sender, RoutedEventArgs e)
 		{
