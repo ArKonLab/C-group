9b989737710a72ed1682f5bc3a4b6fcca727cd0d
ArKonLab <arkonlab@outlook.com>
2018-01-14 20:09:08 +0300

    Создана статичная переменная

--- OldSovietGames/WaitForIt/AnimationLT.cs
+++ OldSovietGames/WaitForIt/AnimationLT.cs
@@ -18,35 +18,37 @@ namespace WaitForIt
 		DiscreteDoubleKeyFrame keyFrame5;
 		DiscreteDoubleKeyFrame keyFrame6;
 
+		static public int time = 1;
+
 		public AnimationLT(Egg egg)
 		{
 			keyFrame1 = new DiscreteDoubleKeyFrame();
-			keyFrame1.KeyTime = TimeSpan.FromSeconds(1);
+			keyFrame1.KeyTime = TimeSpan.FromSeconds(time);
 			keyFrame1.Value = Canvas.GetTop(egg);
 			KeyFrames.Add(keyFrame1);
 
 			keyFrame2 = new DiscreteDoubleKeyFrame();
-			keyFrame2.KeyTime = TimeSpan.FromSeconds(2);
+			keyFrame2.KeyTime = TimeSpan.FromSeconds(time*2);
 			keyFrame2.Value = Canvas.GetTop(egg) + 2;
 			KeyFrames.Add(keyFrame2);
 
 			keyFrame3 = new DiscreteDoubleKeyFrame();
-			keyFrame3.KeyTime = TimeSpan.FromSeconds(3);
+			keyFrame3.KeyTime = TimeSpan.FromSeconds(time*3);
 			keyFrame3.Value = Canvas.GetTop(egg) + 23;
 			KeyFrames.Add(keyFrame3);
 
 			keyFrame4 = new DiscreteDoubleKeyFrame();
-			keyFrame4.KeyTime = TimeSpan.FromSeconds(4);
+			keyFrame4.KeyTime = TimeSpan.FromSeconds(time*4);
 			keyFrame4.Value = Canvas.GetTop(egg) + 22;
 			KeyFrames.Add(keyFrame4);
 
 			keyFrame5 = new DiscreteDoubleKeyFrame();
-			keyFrame5.KeyTime = TimeSpan.FromSeconds(5);
+			keyFrame5.KeyTime = TimeSpan.FromSeconds(time*5);
 			keyFrame5.Value = Canvas.GetTop(egg) + 43;
 			KeyFrames.Add(keyFrame5);
 
 			keyFrame6 = new DiscreteDoubleKeyFrame();
-			keyFrame6.KeyTime = TimeSpan.FromSeconds(6);
+			keyFrame6.KeyTime = TimeSpan.FromSeconds(time*6);
 			keyFrame6.Value = Canvas.GetTop(egg) + 53;
 			KeyFrames.Add(keyFrame6);
 		}
