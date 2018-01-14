574b42a209c880e60b75f1683d75d13985d65d14
kertak90 <35306203+kertak90@users.noreply.github.com>
2018-01-14 21:25:10 +0300

    Класс волк пока сырой

---
+++ OldSovietGames/WaitForIt/Woolf.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaitForIt
+{
+    class Woolf
+    {
+        public static void woolfImage(short keyIndex)
+        {
+            switch(keyIndex)
+            {
+                case 1:         //Нажатие левой верхней кнопки
+                                //Вставляем изображение волка лево вверх
+
+                    break;
+                case 2:         //Нажатие левой нижней кнопки
+
+                    break;
+                case 3:         //Нажатие правой верхней кнопки
+
+                    break;
+                case 4:         //Нажатие правой нижней кнопки
+
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
