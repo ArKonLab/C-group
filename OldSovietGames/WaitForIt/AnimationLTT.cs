f40552f162c6fe7f6de03b0f5b0ae162158eb681
ArKonLab <arkonlab@outlook.com>
2018-01-15 02:31:44 +0300

    Небольшие доработки

--- OldSovietGames/WaitForIt/AnimationLTT.cs
+++ OldSovietGames/WaitForIt/AnimationLTT.cs
@@ -23,32 +23,31 @@ namespace WaitForIt
 		public AnimationLTT(Egg egg)
 		{
 			keyFrame1 = new DiscreteDoubleKeyFrame();
-			keyFrame1.KeyTime = TimeSpan.FromSeconds(MainWindow.time);
 			keyFrame1.Value = Canvas.GetTop(egg);
 			KeyFrames.Add(keyFrame1);
 
 			keyFrame2 = new DiscreteDoubleKeyFrame();
-			keyFrame2.KeyTime = TimeSpan.FromSeconds(MainWindow.time * 2);
+			keyFrame2.KeyTime = TimeSpan.FromSeconds(MainWindow.time * 1);
 			keyFrame2.Value = Canvas.GetTop(egg) + 2;
 			KeyFrames.Add(keyFrame2);
 
 			keyFrame3 = new DiscreteDoubleKeyFrame();
-			keyFrame3.KeyTime = TimeSpan.FromSeconds(MainWindow.time * 3);
+			keyFrame3.KeyTime = TimeSpan.FromSeconds(MainWindow.time * 2);
 			keyFrame3.Value = Canvas.GetTop(egg) + 23;
 			KeyFrames.Add(keyFrame3);
 
 			keyFrame4 = new DiscreteDoubleKeyFrame();
-			keyFrame4.KeyTime = TimeSpan.FromSeconds(MainWindow.time * 4);
+			keyFrame4.KeyTime = TimeSpan.FromSeconds(MainWindow.time * 3);
 			keyFrame4.Value = Canvas.GetTop(egg) + 22;
 			KeyFrames.Add(keyFrame4);
 
 			keyFrame5 = new DiscreteDoubleKeyFrame();
-			keyFrame5.KeyTime = TimeSpan.FromSeconds(MainWindow.time * 5);
+			keyFrame5.KeyTime = TimeSpan.FromSeconds(MainWindow.time * 4);
 			keyFrame5.Value = Canvas.GetTop(egg) + 43;
 			KeyFrames.Add(keyFrame5);
 
 			keyFrame6 = new DiscreteDoubleKeyFrame();
-			keyFrame6.KeyTime = TimeSpan.FromSeconds(MainWindow.time * 6);
+			keyFrame6.KeyTime = TimeSpan.FromSeconds(MainWindow.time * 5);
 			keyFrame6.Value = Canvas.GetTop(egg) + 53;
 			KeyFrames.Add(keyFrame6);
 		}
