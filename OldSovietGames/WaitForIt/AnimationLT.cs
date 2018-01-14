f6a3eeace7d70afc7fa98d17aef19bc6e4cdf79e
ArKonLab <arkonlab@outlook.com>
2018-01-14 20:23:40 +0300

    1

--- OldSovietGames/WaitForIt/AnimationLT.cs
+++ OldSovietGames/WaitForIt/AnimationLT.cs
@@ -28,27 +28,27 @@ namespace WaitForIt
 			KeyFrames.Add(keyFrame1);
 
 			keyFrame2 = new DiscreteDoubleKeyFrame();
-			keyFrame2.KeyTime = TimeSpan.FromSeconds(time*2);
+			keyFrame2.KeyTime = TimeSpan.FromSeconds(MainWindow.time * 2);
 			keyFrame2.Value = Canvas.GetTop(egg) + 2;
 			KeyFrames.Add(keyFrame2);
 
 			keyFrame3 = new DiscreteDoubleKeyFrame();
-			keyFrame3.KeyTime = TimeSpan.FromSeconds(time*3);
+			keyFrame3.KeyTime = TimeSpan.FromSeconds(MainWindow.time * 3);
 			keyFrame3.Value = Canvas.GetTop(egg) + 23;
 			KeyFrames.Add(keyFrame3);
 
 			keyFrame4 = new DiscreteDoubleKeyFrame();
-			keyFrame4.KeyTime = TimeSpan.FromSeconds(time*4);
+			keyFrame4.KeyTime = TimeSpan.FromSeconds(MainWindow.time * 4);
 			keyFrame4.Value = Canvas.GetTop(egg) + 22;
 			KeyFrames.Add(keyFrame4);
 
 			keyFrame5 = new DiscreteDoubleKeyFrame();
-			keyFrame5.KeyTime = TimeSpan.FromSeconds(time*5);
+			keyFrame5.KeyTime = TimeSpan.FromSeconds(MainWindow.time * 5);
 			keyFrame5.Value = Canvas.GetTop(egg) + 43;
 			KeyFrames.Add(keyFrame5);
 
 			keyFrame6 = new DiscreteDoubleKeyFrame();
-			keyFrame6.KeyTime = TimeSpan.FromSeconds(time*6);
+			keyFrame6.KeyTime = TimeSpan.FromSeconds(MainWindow.time * 6);
 			keyFrame6.Value = Canvas.GetTop(egg) + 53;
 			KeyFrames.Add(keyFrame6);
 		}
