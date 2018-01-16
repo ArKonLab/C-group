6ec6fbe601734195de0c24a9dd3a2f390566789a
ArKonLab <arkonlab@outlook.com>
2018-01-16 08:41:29 +0300

    Добавлен обработчик события Compleeted

--- OldSovietGames/WaitForIt/Egg.cs
+++ OldSovietGames/WaitForIt/Egg.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -18,6 +19,9 @@ namespace WaitForIt
 			// подключаем изображение
 			Source = new BitmapImage( new Uri(@"img/egg_left_down_1.png", UriKind.RelativeOrAbsolute));
 
+			AnimationLTT animationLTT;
+			AnimationLTL animationLTL;
+
 			Random random = new Random();
 			position = random.Next(1, 5);
 			
@@ -28,9 +32,10 @@ namespace WaitForIt
 					// начальные координаты для левого верхнего положения
 					Canvas.SetLeft(this, 225);
 					Canvas.SetTop(this, 207);
-					AnimationLTT animationLTT = new AnimationLTT(this);
+					animationLTT = new AnimationLTT(this);
+					animationLTT.Completed += Animation_Completed;
 					this.BeginAnimation(Canvas.TopProperty, animationLTT);
-					AnimationLTL animationLTL = new AnimationLTL(this);
+					animationLTL = new AnimationLTL(this);
 					this.BeginAnimation(Canvas.LeftProperty, animationLTL);
 					break;
 
@@ -39,6 +44,7 @@ namespace WaitForIt
 					Canvas.SetLeft(this, 225);
 					Canvas.SetTop(this, 330);
                     animationLTT = new AnimationLTT(this);
+					animationLTT.Completed += Animation_Completed;
 					this.BeginAnimation(Canvas.TopProperty, animationLTT);
 					animationLTL = new AnimationLTL(this);
 					this.BeginAnimation(Canvas.LeftProperty, animationLTL);
@@ -49,6 +55,7 @@ namespace WaitForIt
 					Canvas.SetLeft(this, 875);
 					Canvas.SetTop(this, 205);
                     AnimationRT animationRT = new AnimationRT(this);
+					animationRT.Completed += Animation_Completed;
                     this.BeginAnimation(Canvas.TopProperty, animationRT);
                     AnimationRL animationRL = new AnimationRL(this);
                     this.BeginAnimation(Canvas.LeftProperty, animationRL);
@@ -59,16 +66,28 @@ namespace WaitForIt
 					Canvas.SetLeft(this, 875);
 					Canvas.SetTop(this, 327);
                     animationRT = new AnimationRT(this);
-                    this.BeginAnimation(Canvas.TopProperty, animationRT);
+					animationRT.Completed += Animation_Completed;
+					this.BeginAnimation(Canvas.TopProperty, animationRT);
                     animationRL = new AnimationRL(this);
                     this.BeginAnimation(Canvas.LeftProperty, animationRL);
                     break;
 
 				default:
+					Canvas.SetLeft(this, 225);
+					Canvas.SetTop(this, 207);
+					animationLTT = new AnimationLTT(this);
+					this.BeginAnimation(Canvas.TopProperty, animationLTT);
+					animationLTL = new AnimationLTL(this);
+					this.BeginAnimation(Canvas.LeftProperty, animationLTL);
 					break;
 			}
 			 
 			MainWindow.canvas2.Children.Add(this);
+		}
+
+		private void Animation_Completed(object sender, EventArgs e)
+		{
+			MainWindow.canvas2.Children.Remove(this);
 		}
 
 		public int GetPosition()
